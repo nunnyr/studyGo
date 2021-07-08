680ed68ed01936a8dc8b66b45cd9227731354d76
Nunny <nunnyreyes@gmail.com>
2021-07-08 17:17:55 -0400

    chapter2

--- chapter1/main.go
+++ chapter1/main.go
@@ -1,9 +1,33 @@
+//this is known as package declaration and every Go program starts with it.
+//packages are Go's way of organizing and reusing code
+//Two types of Go programs: executables and libraries
+
+/*
+	Executable applications are the kinds of programs that we can run directly from the terminal.
+	Libraries are collections of code that we package togetether so that
+	we can use them in other programs.
+	//edits
+
+*/
+
+/*
+	import keyword is how we include code from other packages to use with our program.
+	fmt package is short for format. it Implements formatting for input and output.
+	Given what we just learned about packages file would contain
+	at the top of them.
+
+*/
 package main
 
 import "fmt"
 
 //this is a comment
 
-func main(){
-	fmt.Println("Hello, World")
+func main() {
+	fmt.Println(32132 * 42452)
+
 }
+
+// func math() {
+// 	fmt.Println(32132 * 42452)
+// }
