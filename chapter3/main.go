c44b00c67de28cd3861e035003e2aa781ca616cb
Nunny <nunnyreyes@gmail.com>
2021-07-08 18:01:31 -0400

    variable naming

---
+++ chapter3/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+//Creating a new variable is common
+// x := "Hello, World"
+
+func main() {
+	var x string
+	x = "first"
+	fmt.Println(x)
+	x = "second"
+	fmt.Println(x)
+}
